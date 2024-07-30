19da10dc6f96be476fc03f44c69deaeea68ab3bb
João Pedro Thomé <jpsthome@gmail.com>
2024-07-29 22:37:45 -0300

    feature(user): creates user data-access library

---
+++ libs/modules/data-access/user/src/index.ts
@@ -0,0 +1,3 @@
+export * from './lib/models/user.model';
+export * from './lib/users/users.service';
+export * from './lib/mocks/users.mock';
---
+++ libs/modules/data-access/user/src/lib/users/users.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { User } from '../models/user.model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsersService {
+  readonly apiUrl = 'https://65009f9718c34dee0cd53786.mockapi.io';
+
+  private http = inject(HttpClient);
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(`${this.apiUrl}/users`);
+  }
+}
