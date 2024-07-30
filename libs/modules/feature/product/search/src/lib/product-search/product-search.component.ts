a0ba79268e67af15695ab482bbf3c25c012c4518
João Pedro Thomé <jpsthome@gmail.com>
2024-07-29 21:33:32 -0300

    feat(ecommerce-home): adds link to admin panel

--- libs/modules/feature/product/search/src/lib/product-search/product-search.component.ts
+++ libs/modules/feature/product/search/src/lib/product-search/product-search.component.ts
@@ -5,8 +5,7 @@ import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { RouterModule } from '@angular/router';
-import { ProductSearchService } from '@ecommerce-mentoria-2/product-data-access';
-import { Product } from 'modules/data-access/product/src/lib/models/product.model';
+import { Product, ProductSearchService } from '@ecommerce-mentoria-2/product-data-access';
 import {
   Observable,
   debounceTime,
