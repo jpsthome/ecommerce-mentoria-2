caf8779297818f8722ccd017f377c04952e6edfc
João Pedro Thomé <jpsthome@gmail.com>
2024-08-01 19:34:33 -0300

    feat(user-table): creates user table component and implements it on home

--- libs/modules/feature/panel-home/src/lib/panel-home/panel-home.component.ts
+++ libs/modules/feature/panel-home/src/lib/panel-home/panel-home.component.ts
@@ -1,11 +1,17 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UsersService } from '@ecommerce-mentoria-2/user';
+import { UserTableComponent } from '@ecommerce-mentoria-2/user-ui';
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, UserTableComponent],
   templateUrl: './panel-home.component.html',
   styleUrl: './panel-home.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PanelHomeComponent {}
+export class PanelHomeComponent {
+  private userService = inject(UsersService);
+
+  users$ = this.userService.getUsers();
+}
