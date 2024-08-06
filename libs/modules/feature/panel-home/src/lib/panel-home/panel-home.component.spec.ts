7286e1f7b0c1cc58b36ab388a16fb5862a628d0c
Jhonattan Loli Mazzuco <jhonattan.mazzuco.ext@esferaenergia.com.br>
2024-08-06 20:42:56 -0300

    feat(panel-home): adds loading state when fetch users

--- libs/modules/feature/panel-home/src/lib/panel-home/panel-home.component.spec.ts
+++ libs/modules/feature/panel-home/src/lib/panel-home/panel-home.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { UsersService } from '@ecommerce-mentoria-2/user';
+import { UserTableComponent } from '@ecommerce-mentoria-2/user-ui';
 import { PanelHomeComponent } from './panel-home.component';
 
 describe('PanelHomeComponent', () => {
@@ -7,7 +9,18 @@ describe('PanelHomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PanelHomeComponent],
+      imports: [
+        PanelHomeComponent,
+        UserTableComponent,
+      ],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: {
+            getUsers: jest.fn()
+          }
+        }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(PanelHomeComponent);
@@ -18,4 +31,20 @@ describe('PanelHomeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should active loading template', () => {
+    const loadingEl = fixture.nativeElement.querySelector('.loading');
+
+    expect(loadingEl).toBeTruthy();
+    expect(loadingEl.textContent.trim()).toBe('Buscando usuários');
+  });
+
+  // it('should render user table', () => {
+  //   jest.spyOn(component['userService'], 'getUsers')
+  //     .mockReturnValue(of(usersMock));
+  //
+  //   const userTableEl = fixture.nativeElement.querySelector('ecommerce-mentoria-2-user-table');
+  //
+  //   expect(userTableEl).toBeTruthy();
+  // });
 });
