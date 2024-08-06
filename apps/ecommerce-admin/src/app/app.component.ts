591b3351f1655c3247e33e47d3463726b59537af
Jhonattan Loli Mazzuco <jhonattan.mazzuco.ext@esferaenergia.com.br>
2024-08-05 23:47:47 -0300

    feat(ecommerce-admin): removed NxWelcomeComponent

--- apps/ecommerce-admin/src/app/app.component.ts
+++ apps/ecommerce-admin/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { LayoutModule } from '@ecommerce-mentoria-2/layout';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, LayoutModule],
+  imports: [RouterModule, LayoutModule],
   selector: 'ecommerce-mentoria-2-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
