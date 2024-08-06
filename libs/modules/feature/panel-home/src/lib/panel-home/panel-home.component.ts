7c2ce1ed7c3149573c4eddfce2e2a7bd46513563
Jhonattan Loli Mazzuco <jhonattan.mazzuco.ext@esferaenergia.com.br>
2024-08-06 20:42:56 -0300

    feat(panel-home): adds loading state when fetch users

--- libs/modules/feature/panel-home/src/lib/panel-home/panel-home.component.ts
+++ libs/modules/feature/panel-home/src/lib/panel-home/panel-home.component.ts
@@ -1,14 +1,15 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
 import { UsersService } from '@ecommerce-mentoria-2/user';
 import { UserTableComponent } from '@ecommerce-mentoria-2/user-ui';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, UserTableComponent],
+  imports: [CommonModule, UserTableComponent, MatProgressSpinner],
   templateUrl: './panel-home.component.html',
   styleUrl: './panel-home.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PanelHomeComponent {
   private userService = inject(UsersService);
