4a99ffca588fabb08a23f0424a2cfc92d41d29ad
Jhonattan Loli Mazzuco <jhonattan.mazzuco.ext@esferaenergia.com.br>
2024-08-05 23:51:16 -0300

    feat(ecommerce-admin): adds pt-br locale

--- apps/ecommerce-admin/src/app/app.config.ts
+++ apps/ecommerce-admin/src/app/app.config.ts
@@ -1,13 +1,22 @@
-import { ApplicationConfig } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import { provideHttpClient } from '@angular/common/http';
+
+import ptBr from '@angular/common/locales/pt';
+import { ApplicationConfig, LOCALE_ID } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient } from '@angular/common/http';
+
+registerLocaleData(ptBr)
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(appRoutes),
     provideAnimationsAsync(),
     provideHttpClient(),
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt'
+    }
   ],
 };
