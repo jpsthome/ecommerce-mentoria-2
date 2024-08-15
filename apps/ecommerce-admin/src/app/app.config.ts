96a42cb835bf259c7f6c965e590f5e6e27470a80
João Pedro Thomé <jpsthome@gmail.com>
2024-08-14 23:49:16 -0300

    feat(painel-administrativo): implements http error interceptor

--- apps/ecommerce-admin/src/app/app.config.ts
+++ apps/ecommerce-admin/src/app/app.config.ts
@@ -1,22 +1,23 @@
 import { registerLocaleData } from '@angular/common';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import ptBr from '@angular/common/locales/pt';
 import { ApplicationConfig, LOCALE_ID } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { httpErrorsInterceptor } from './interceptors/http-errors.interceptor';
 
-registerLocaleData(ptBr)
+registerLocaleData(ptBr);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(appRoutes),
     provideAnimationsAsync(),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([httpErrorsInterceptor])),
     {
       provide: LOCALE_ID,
-      useValue: 'pt'
-    }
+      useValue: 'pt',
+    },
   ],
 };
