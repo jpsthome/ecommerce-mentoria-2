4eed871fcbc0a7fc78cb72c41c2a7f9ff8713dd1
João Pedro Thomé <jpsthome@gmail.com>
2024-08-21 11:00:43 -0300

    ci: updates environment.prod url paths

--- apps/ecommerce-admin/src/environments/environment.prod.ts
+++ apps/ecommerce-admin/src/environments/environment.prod.ts
@@ -1,6 +1,4 @@
 export const environment = {
-  admin_url:
-    'https://ecommerce-admin-4pi3rkre1-joao-pedro-thomes-projects.vercel.app/',
-  ecommerce_url:
-    'https://ecommerce-mentoria-2-boc6a07sb-joao-pedro-thomes-projects.vercel.app/home',
+  admin_url: 'https://ecommerce-admin-one-xi.vercel.app/home',
+  ecommerce_url: 'https://ecommerce-mentoria-2-beta.vercel.app/home',
 };
