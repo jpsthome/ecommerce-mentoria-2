4193cdfecabd31177035b8e467d8ac8f6b426379
Jhonattan Loli Mazzuco <jhonattan.mazzuco.ext@esferaenergia.com.br>
2024-08-20 21:33:57 -0300

    fix: fix merge conflict

--- libs/modules/data-access/auth/src/lib/guards/email-inserted.guard.ts
+++ libs/modules/data-access/auth/src/lib/guards/email-inserted.guard.ts
@@ -1,13 +1,10 @@
 import { inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { map } from 'rxjs';
 import { AuthService } from '../services';
 
 export function emailInsertedGuard() {
   const router = inject(Router);
-  const email$ = inject(AuthService).email$;
+  const email = inject(AuthService).email;
 
-  return email$.pipe(
-    map((email) => (!email?.length ? router.navigateByUrl('/login/email') : true))
-  );
+  return !email()?.length ? router.navigateByUrl('/login/email') : true;
 }
